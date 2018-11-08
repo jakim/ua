293b21f64e9e129f9ea2161f1514d05b06d1e703
Paweł Jakimowski <pawel@jakimowski.info>
2018-11-08 20:45:02 +0100

    chg: curl

--- UserAgent.php
+++ UserAgent.php
@@ -10,6 +10,8 @@ namespace jakim\ua;
 
 class UserAgent
 {
+    public $default = 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19';
+
     public $src = [
         'https://developers.whatismybrowser.com/useragents/explore/software_type_specific/web-browser/1',
         'https://developers.whatismybrowser.com/useragents/explore/software_type_specific/web-browser/2',
@@ -31,7 +33,14 @@ class UserAgent
         $items = [];
         $dom = new \DOMDocument();
         foreach ($this->src as $src) {
-            @$dom->loadHTMLFile($src);
+
+            $ch = curl_init($src);
+            curl_setopt($ch, CURLOPT_USERAGENT, $this->default);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            $src = curl_exec($ch);
+
+            @$dom->loadHTML($src);
             $xpath = new \DOMXPath($dom);
             $rows = $xpath->query('//td[contains(@class, \'useragent\')]');
             foreach ($rows as $row) {
