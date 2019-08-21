5140db5ae97cefd577d80e8e8a59506b7ebdc02b
Paweł Jakimowski <pawel@jakimowski.info>
2019-08-21 06:21:03 +0200

    chg: default useragent

--- UserAgent.php
+++ UserAgent.php
@@ -10,7 +10,7 @@ namespace jakim\ua;
 
 class UserAgent
 {
-    public $default = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36';
+    public $default = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36';
 
     public $src = [
         'https://developers.whatismybrowser.com/useragents/explore/software_type_specific/web-browser/1',
